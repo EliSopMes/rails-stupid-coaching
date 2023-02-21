5b970365c65fa4d1f7417bdcbdc7dfa5c17ef29d
Elisabeth Messmer <elisabethsophiem@web.de>
2023-02-21 09:12:10 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'questions', to: 'questions#ask'
+  get 'answers', to: 'answers#answer'
 end
